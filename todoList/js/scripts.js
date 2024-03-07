4d8e07e592875e3416d9bf0bfbf4b732e8645280
lms-souza <leonardomtzsouza@rede.ulbra.br>
2024-03-06 22:45:32 -0300

    att projeto fork

--- todoList/js/scripts.js
+++ todoList/js/scripts.js
@@ -1,39 +1,80 @@
 // Clase
 
 class ToDo {
-
+  Texto;
+  Prioridade;
+  Feito = false;
+  constructor(texto, prioridade) {
+    this.Texto = texto;
+    this.Prioridade = prioridade;
+  }
 }
-
-// Array
 
 
 //funções projeto
 
-function CriarToDo() {
-
+function CriarToDo(texto, prioridade, array) {
+  let objetoTodo = new ToDo(texto, prioridade);
+  if (!array.some((x) => x.Texto == texto)) array.push(objetoTodo);
+  return objetoTodo;
 }
 
-function AtualizarToDo() {
-
+function AtualizarToDo(textoAntigo, textoNovo, array) {
+  let atualizado = false
+  array.forEach(todo => {
+    if (todo.Texto == textoAntigo) {
+      todo.Texto = textoNovo
+      atualizado = true
+    }
+  })
+  return atualizado;
 }
 
-function ConcluirToDo() {
-
+function ConcluirToDo(array, texto) {
+  let concluido = false
+  array.forEach(todo => {
+    if (todo.Texto == texto) {
+      if (todo.Feito) 
+        todo.Feito = false
+      else {
+        todo.Feito = true
+      }
+      concluido = true
+    }   
+  })
+  return concluido;
 }
 
-function ExcluirToDo() {
-
+function ExcluirToDo(array, texto) {
+  let index;
+  let removido = false;
+  array.forEach((todo) => {
+    if (todo.Texto == texto) {
+      index = array.indexOf(todo);
+      removido = true;
+    }
+  });
+  array.splice(index, 1);
+  return removido;
 }
 
-function PesquisarToDo() {
- 
+function PesquisarToDo(array, texto) {
+  let pesquisa = false
+  array.forEach((todo) => {
+    if (todo.Texto.includes(texto)) {
+      pesquisa = true;
+    }
+  });
+  return pesquisa;
 }
 
-function OrdenarCrescente() {
-  
+function OrdenarCrescente(array) {
+  array.sort((a, b) => a.Prioridade - b.Prioridade);
+  return array;
 }
-function OrdenarDecrescente() {
-  
+function OrdenarDecrescente(array) {
+  array.sort((a, b) => b.Prioridade - a.Prioridade);
+  return array;
 }
 
 // Seleção de elementos
@@ -49,12 +90,10 @@ const eraseBtn = document.querySelector("#erase-button");
 const filterBtn = document.querySelector("#filter-select");
 
 let oldInputValue;
-
-
-
+let arrayTodos = [];
 // Funções
 const saveTodo = (text, rating, done = 0, save = 1) => {
-  let objetoTodo = CriarToDo(text, rating, arrayTodos)
+  let objetoTodo = CriarToDo(text, rating, arrayTodos);
 
   const todo = document.createElement("div");
   todo.classList.add("todo");
@@ -95,8 +134,6 @@ const saveTodo = (text, rating, done = 0, save = 1) => {
 
   todoInput.value = "";
   todoInput2.value = "";
-
-
 };
 
 const toggleForms = () => {
@@ -107,16 +144,15 @@ const toggleForms = () => {
 
 const updateTodo = (text) => {
   const todos = document.querySelectorAll(".todo");
-  let targetTodo
+  let targetTodo;
   todos.forEach((todo) => {
     let todoTitle = todo.querySelector("h3");
     if (todoTitle.innerText === oldInputValue) {
-      targetTodo = todoTitle
+      targetTodo = todoTitle;
     }
-
   });
 
-  let atualizado = AtualizarToDo(targetTodo.innerText, text,arrayTodos)
+  let atualizado = AtualizarToDo(targetTodo.innerText, text, arrayTodos);
 
   if (atualizado) {
     targetTodo.innerText = text;
@@ -128,7 +164,7 @@ const updateTodo = (text) => {
 const getSearchedTodos = (search) => {
   const todos = document.querySelectorAll(".todo");
 
-  let pesquisa = PesquisarToDo(arrayTodos, search)
+  let pesquisa = PesquisarToDo(arrayTodos, search);
 
   if (pesquisa) {
     todos.forEach((todo) => {
@@ -140,10 +176,8 @@ const getSearchedTodos = (search) => {
         todo.style.display = "none";
       }
     });
-  };
-}
-
-
+  }
+};
 
 const filterTodos = (filterValue) => {
   const todos = document.querySelectorAll(".todo");
@@ -151,20 +185,24 @@ const filterTodos = (filterValue) => {
   switch (filterValue) {
     case "cresc":
       todos.forEach((todo) => {
-        todo.remove()
-        removeTodoLocalStorage(todo.querySelector("h3").innerText)
-      })
-      arrayTodos = OrdenarCrescente(arrayTodos)
-      arrayTodos.forEach((todo) => saveTodo(todo.Texto, todo.Prioridade, done = 0, save = 1))
+        todo.remove();
+        removeTodoLocalStorage(todo.querySelector("h3").innerText);
+      });
+      arrayTodos = OrdenarCrescente(arrayTodos);
+      arrayTodos.forEach((todo) =>
+        saveTodo(todo.Texto, todo.Prioridade, (done = 0), (save = 1))
+      );
       break;
 
     case "decresc":
       todos.forEach((todo) => {
-        todo.remove()
-        removeTodoLocalStorage(todo.querySelector("h3").innerText)
-      })
-      arrayTodos = OrdenarDecrescente(arrayTodos)
-      arrayTodos.forEach((todo) => saveTodo(todo.Texto, todo.Prioridade, done = 0, save = 1))
+        todo.remove();
+        removeTodoLocalStorage(todo.querySelector("h3").innerText);
+      });
+      arrayTodos = OrdenarDecrescente(arrayTodos);
+      arrayTodos.forEach((todo) =>
+        saveTodo(todo.Texto, todo.Prioridade, (done = 0), (save = 1))
+      );
       break;
 
     default:
@@ -194,8 +232,8 @@ document.addEventListener("click", (e) => {
   }
 
   if (targetEl.classList.contains("finish-todo")) {
-    todoTitle = parentEl.querySelector("h3").innerText
-    let concluido = ConcluirToDo(arrayTodos, todoTitle)
+    todoTitle = parentEl.querySelector("h3").innerText;
+    let concluido = ConcluirToDo(arrayTodos, todoTitle);
     if (concluido) {
       parentEl.classList.toggle("done");
       updateTodoStatusLocalStorage(todoTitle);
@@ -203,15 +241,14 @@ document.addEventListener("click", (e) => {
   }
 
   if (targetEl.classList.contains("remove-todo")) {
-    todoTitle = parentEl.querySelector("h3").innerText
-    let removido = ExcluirToDo(arrayTodos, todoTitle)
+    todoTitle = parentEl.querySelector("h3").innerText;
+    let removido = ExcluirToDo(arrayTodos, todoTitle);
     if (removido) {
       parentEl.remove();
 
       // Utilizando dados da localStorage
       removeTodoLocalStorage(todoTitle);
     }
-
   }
 
   if (targetEl.classList.contains("edit-todo")) {
